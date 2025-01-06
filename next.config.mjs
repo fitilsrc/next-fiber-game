49691b626a795273522b3aa9790ab22a04f2eec0
Serhii Dovhyi <dovhyi.serhii@gmail.com>
2025-01-07 01:26:07 +0200

    glsl and shader material

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: ['raw-loader', 'glslify', 'glslify-loader'],
+    })
+    return config
+  },
+};
 
 export default nextConfig;
--- src/features/terrain/components/terrain.tsx
+++ src/features/terrain/components/terrain.tsx
@@ -1,26 +1,71 @@
-import { memo } from "react";
+'use client';
+
+import { memo, useRef } from "react";
 
 import * as THREE from "three";
-import { useLoader } from "@react-three/fiber";
+import { extend, useLoader, useThree, type ShaderMaterialProps } from "@react-three/fiber";
+import { shaderMaterial } from "@react-three/drei";
+
+import fragmentShader from './terrain.frag'
+import vertexShader from './terrain.vert'
+
+type Uniforms = {
+  uTime: number
+  uAspectRatio: number
+  uScrollOffset: number
+  uLightColour: THREE.Color
+  uDarkColour: THREE.Color
+  uTexture: THREE.Texture | null
+}
+
+const INITIAL_UNIFORMS: Uniforms = {
+  uTime: 0,
+  uAspectRatio: 1,
+  uScrollOffset: 0,
+  uLightColour: new THREE.Color('#2E2A37').convertLinearToSRGB(),
+  uDarkColour: new THREE.Color('#0A090C').convertLinearToSRGB(),
+  uTexture: null,
+}
+
+const RippleShaderMaterial = shaderMaterial(
+  // uniform
+  INITIAL_UNIFORMS,
+  vertexShader,
+  fragmentShader
+)
+
+extend({ RippleShaderMaterial });
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      waveShaderMaterial: ShaderMaterialProps & Uniforms
+    }
+  }
+}
 
 export const Terrain = () => {
-  const water = useLoader(THREE.TextureLoader, "/assets/textures/water.avif");
-
-  water.repeat = new THREE.Vector2(1, 1);
-  water.wrapS = THREE.RepeatWrapping;
-  water.wrapT = THREE.RepeatWrapping;
-
+  const { viewport } = useThree();
+  const texture = useLoader(THREE.TextureLoader, "/assets/textures/water.avif");
+  const shader = useRef<THREE.ShaderMaterial & Partial<Uniforms>>(null);
+  
   return (
     <mesh
       position={[0, -0.1, 0]}
       receiveShadow
     >
-      <cylinderGeometry
+      <cylinderBufferGeometry
         args={[30, 30, 1.1]}
       />
-      <meshStandardMaterial
-        color={"#4477ff"}
-        flatShading
+      <waveShaderMaterial
+        key={RippleShaderMaterial.key}
+        ref={shader}
+        uTime={0}
+        uScrollOffset={0}
+        uAspectRatio={viewport.aspect}
+        uLightColour={INITIAL_UNIFORMS.uLightColour}
+        uDarkColour={INITIAL_UNIFORMS.uDarkColour}
+        uTexture={texture}
       />
     </mesh>
   );
