209a36252f2cb6ca107bd3b1415892d4e0944b3a
fitilsrc <dovhyi.serhii@gmail.com>
2025-01-15 00:35:39 +0200

    feat: added store and active tile hexagon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import { ActiveTileStoreProvider } from "@/components/providers/active-tile-provider";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ActiveTileStoreProvider>
+          {children}
+        </ActiveTileStoreProvider>
       </body>
     </html>
   );
